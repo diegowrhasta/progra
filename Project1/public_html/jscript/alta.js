a2e523a08e25cf8e155f2abe0adedbfb4f4506d1
Diego Balderrama <diegowrhasta@gmail.com>
2019-11-26 20:24:03 -0400

    Worked some typos

--- Project1/public_html/jscript/alta.js
+++ Project1/public_html/jscript/alta.js
@@ -4,11 +4,11 @@ function fParams( )
   f = document.forms[0];
   
   if(EsVacio( f.cod.value ))
-    Mensaje = Mensaje + 'Verifique el codigo \n';
+    Mensaje = Mensaje + 'Verifique el Código \n';
   if(EsVacio( f.descripcion.value ))
-    Mensaje = Mensaje + 'Verifique la Descripcion \n';
+    Mensaje = Mensaje + 'Verifique la Descripción \n';
   if(EsVacio( f.region.value ))
-    Mensaje = Mensaje + 'Verifique la Region \n';
+    Mensaje = Mensaje + 'Verifique la Región \n';
 
   if( Mensaje == "" )
     return true;
